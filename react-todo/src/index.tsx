58000005ad4ae7d54fce8ca130ffda70b7d1d2c8
thomas1117 <thomasshannon117@yahoo.com>
2022-05-20 22:43:22 -0700

    add logger apollo link

--- react-todo/src/index.tsx
+++ react-todo/src/index.tsx
@@ -1,13 +1,31 @@
+// @ts-ignore
+import apolloLogger from 'apollo-link-logger'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  HttpLink,
+} from '@apollo/client'
 import TodoGQL from './gql-example/TodoGQL'
+
+// https://github.com/apollographql/apollo-client/issues/6850
+
+const links = [new HttpLink({ uri: 'http://localhost:3001/graphql' })]
+// logging apollo logger conditionally
+// localStorage.getItem('logDev')
+if (true) {
+  links.unshift(apolloLogger)
+}
 
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql',
   cache: new InMemoryCache(),
+  link: ApolloLink.from(links),
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
